2c0c670e313dc24df55da8657f34238028d562ea
PavanKuchana <pavankuchana007@gmail.com>
2021-10-04 09:45:49 +0000

    Blog List React JS

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      className="user-info-image"
+      alt="profile"
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+    />
+    <h1 className="username">Wade Warren</h1>
+    <p className="info">Software developer at UK</p>
+  </div>
+)
+
+export default UserInfo
